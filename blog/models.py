db27020aaa59698bc8b6495d3dc09982d1d1c6c8
Olgica Djuric <olgica.djuric@ntsystems.rs>
2015-10-05 17:34:10 +0200

    Added image_url field in models and forms for the Post.

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    image_url = models.CharField(max_length=25, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
