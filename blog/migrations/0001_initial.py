aafadfb9696b0d7b7cab7d5194ce17cee7ff345c
Olgica Djuric <olgica.djuric@ntsystems.rs>
2015-10-09 11:44:53 +0200

    Reset migrations.

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 import django.utils.timezone
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
@@ -14,13 +14,22 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Lesson',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('lesson_type', models.CharField(max_length=800)),
+                ('lesson_description', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=200)),
                 ('text', models.TextField()),
                 ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
                 ('published_date', models.DateTimeField(null=True, blank=True)),
+                ('image_name', models.ImageField(null=True, upload_to=b'img', blank=True)),
                 ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
